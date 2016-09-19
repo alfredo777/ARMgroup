35417145dd466122e0de7189ccd6d0e6134003a2
alfredo777 <jose_alfredo@cursa.me>
2016-09-19 07:11:30 -0500

    cambios ne seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 @admin = Admin.create(email: "alfredo@rockstars.mx", password: "123456789")
 @admin2 = Admin.create(email: "carolina.cortes@research-ss.com", password: "carolina123")
 
+@admina = Admin.create(email: "arodriguez@research-ss.com", password: "antonio73")
+@adminb = Admin.create(email: "elba.mejia@research-ss.com", password: "elva84")
+@adminc = Admin.create(email: "sergio.cordova@research-ss.com", password: "sergio984")
+@admind = Admin.create(email: "manuel.diaz@research-ss.com", password: "manuel543")
